ba7e6a114c843e1729b64c43b170385b96cba55a
shakil-lskit <sakil@lskit.com>
2022-04-05 16:00:54 +0600

    chore: basic tesing text added for test shopify store app

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,6 +3,7 @@
 @section('content')
     <!-- You are: (shop domain name) -->
     <p>You are: {{ $shopDomain ?? Auth::user()->name }}</p>
+    <h1>Hello i am from laravel OZ!</h1>
 @endsection
 
 @section('scripts')
