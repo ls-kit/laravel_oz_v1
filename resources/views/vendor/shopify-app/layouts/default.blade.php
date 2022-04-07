cf8c47393a18fa0f31a8d0a37aab637615c816dd
Nazmus Shakib <sakibkhan207@gmail.com>
2022-04-07 12:38:18 +0600

    Bootstrap added

--- resources/views/vendor/shopify-app/layouts/default.blade.php
+++ resources/views/vendor/shopify-app/layouts/default.blade.php
@@ -5,6 +5,7 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>{{ \Osiset\ShopifyApp\Util::getShopifyConfig('app_name') }}</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
         <link rel="stylesheet" href="{{asset('css/app.css')}}" />
         @yield('styles')
     </head>
