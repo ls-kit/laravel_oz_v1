f73243c5b5e041ec7e9e26d6a2862e465bad025e
Nazmus Shakib <sakibkhan207@gmail.com>
2022-04-09 00:12:29 +0600

    js remove to check

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -21,7 +21,7 @@
       </div>
       <canvas class="my-4 w-100" id="myChart" width="900" height="380"></canvas>
     </main>
-    
+
 @endsection
 
 @section('scripts')
@@ -31,10 +31,10 @@
         actions.TitleBar.create(app, { title: 'Welcome' });
     </script>
 
-
+<!-- 
 <script src="https://getbootstrap.com/docs/5.1/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
 
 <script src="https://cdn.jsdelivr.net/npm/feather-icons@4.28.0/dist/feather.min.js" integrity="sha384-uO3SXW5IuS1ZpFPKugNNWqTZRRglnUJK6UAZ/gxOX80nxEkN9NcGZTftn6RzhGWE" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/chart.js@2.9.4/dist/Chart.min.js" integrity="sha384-zNy6FEbO50N+Cg5wap8IKA4M/ZnLJgzc6w2NqACZaK0u0FXfOWRRJOnQtpZun8ha" crossorigin="anonymous"></script>
-<script src="https://getbootstrap.com/docs/5.1/examples/dashboard/dashboard.js"></script>
+<script src="https://getbootstrap.com/docs/5.1/examples/dashboard/dashboard.js"></script> -->
 @endsection
