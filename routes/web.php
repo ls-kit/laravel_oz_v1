ed41b44792c3b55dfa7e591b18f30cf92b0944dd
Nazmus Shakib <sakibkhan207@gmail.com>
2022-04-09 00:30:03 +0600

    route update

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('dashboard');
 })->middleware(['verify.shopify'])->name('home');
+
+Route::view('/products', 'products');
+Route::view('/customers', 'customers');
+Route::view('/settings', 'settings');
