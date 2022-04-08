2a14c4de857de9ec846c51784dad6be611a05c71
Nazmus Shakib <sakibkhan207@gmail.com>
2022-04-09 00:33:01 +0600

    links pages update

--- resources/views/customers.blade.php
+++ resources/views/customers.blade.php
@@ -2,7 +2,6 @@
 
 
 <!-- You are: (shop domain name) -->
-<!-- <p>You are: {{ $shopDomain ?? Auth::user()->name }}</p> -->
 @section('content')
     
     <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
