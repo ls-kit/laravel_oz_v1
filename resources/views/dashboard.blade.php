fa8db5a706a7c422d7e904eba996f46ae6cfe9db
Nazmus Shakib <sakibkhan207@gmail.com>
2022-04-08 23:36:27 +0600

    sidebar include note

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -22,7 +22,9 @@
 <div class="container-fluid">
   <div class="row">
     
+  <h2>sidebar included from nav file </h2>
   @include('partials.navbar')
+
 
     <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
       <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
