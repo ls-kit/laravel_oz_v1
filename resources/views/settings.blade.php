26858a21f3b2029584cdea584fefa7e0115a55c5
Nazmus Shakib <sakibkhan207@gmail.com>
2022-04-09 00:24:47 +0600

    pages create

---
+++ resources/views/settings.blade.php
@@ -0,0 +1,26 @@
+@extends('shopify-app::layouts.default')
+
+
+<!-- You are: (shop domain name) -->
+<p>You are: {{ $shopDomain ?? Auth::user()->name }}</p>
+@section('content')
+    
+    <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+      <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+        <h1 class="h2">Settings</h1>
+        
+      </div>
+      content here
+    </main>
+
+@endsection
+
+@section('scripts')
+    @parent
+
+    <script>
+        actions.TitleBar.create(app, { title: 'Welcome' });
+    </script>
+
+
+@endsection
