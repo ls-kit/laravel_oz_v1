e99ae2184786cc84041e593a0fe9364092895c43
Nazmus Shakib <sakibkhan207@gmail.com>
2022-04-09 00:02:58 +0600

    Link change on navbar

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -2,39 +2,27 @@
       <div class="position-sticky pt-3">
         <ul class="nav flex-column">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">
+            <a class="nav-link @if(Request::path()== '/') active @endif" aria-current="page" href="/">
               <span data-feather="home"></span>
               Dashboard
             </a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">
+            <a class="nav-link @if(Request::path()== 'products') active @endif" href="/products">
               <span data-feather="file"></span>
-              Orders
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">
-              <span data-feather="shopping-cart"></span>
               Products
             </a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">
-              <span data-feather="users"></span>
+            <a class="nav-link @if(Request::path()== 'customers') active @endif" href="/customers">
+              <span data-feather="shopping-cart"></span>
               Customers
             </a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">
-              <span data-feather="bar-chart-2"></span>
-              Reports
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">
-              <span data-feather="layers"></span>
-              Integrations
+            <a class="nav-link @if(Request::path()== 'settings') active @endif" href="/settings">
+              <span data-feather="users"></span>
+              Settings
             </a>
           </li>
         </ul>
