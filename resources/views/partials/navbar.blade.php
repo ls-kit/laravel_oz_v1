2ee13e5de9d5bb5e326096cc543b20c793b4f615
Nazmus Shakib <sakibkhan207@gmail.com>
2022-04-15 15:27:50 +0600

    active update

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,6 +1,6 @@
 <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
-      <div class="position-sticky pt-3 navbar-light">
-        <ul class="nav flex-column navbar-nav">
+      <div class="position-sticky pt-3">
+        <ul class="nav flex-column">
           <li class="nav-item">
             <a class="nav-link @if(Request::path()== '/') active @endif" aria-current="page" href="/">
               <span data-feather="home"></span>
