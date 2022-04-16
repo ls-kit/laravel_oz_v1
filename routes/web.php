685b1311232b420dde7f14274f171c278354a0c2
Nazmus Shakib <sakibkhan207@gmail.com>
2022-04-17 05:49:46 +0600

    up message

--- routes/web.php
+++ routes/web.php
@@ -36,8 +36,6 @@ Route::group(['middleware' => 'verify.shopify'], function () {
 
         $shop = Auth::user();
         $themes = $shop->api()->rest('GET', '/admin/api/2022-04/themes.json');
-        dd($themes);
-
         //search for the right theme id with the main role
         $activeTheme = array_filter(
             $shopThemes->toArray(),
@@ -49,19 +47,22 @@ Route::group(['middleware' => 'verify.shopify'], function () {
 
         $activeThemeId = $activeTheme[0]['id'];
 
-        dd($activeThemeId);
-
         $snippet = "Your snippet code updated 3";
         //Snippet to pass to rest api request
         $data = array(
             'asset'=> [
-                'key' => 'snippets/deidax-wishlist-app-laravel.liquid', 
+                'key' => 'snippets/lskit_codes.liquid', 
                 'value' => $snippet
             ]
         );
 
         $shop->api()->rest('PUT', '/admin/api/2022-04/themes/'.$activeThemeId.'/assets.json', $data);
-        return "success theme";
+        // Save activated shop
+        return Setting::updateOrCreate([
+            'shop_id' => $shop->name,
+            'shop_active_theme_id' => $activeThemeId,
+            'activated' => true,
+        ]) ?  ['message' => 'Theme setup successfully'] : ['message' => 'Theme setup error!'];
 
 
     })->name('test');
