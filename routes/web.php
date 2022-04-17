0a9bb333fbfa0349448c695e1d2098901dbedecc
dev-shakib <sakibkhan207@gmail.com>
2022-04-17 11:16:25 +0600

    create files

--- routes/web.php
+++ routes/web.php
@@ -36,6 +36,8 @@ Route::group(['middleware' => 'verify.shopify'], function () {
 
         $shop = Auth::user();
         $themes = $shop->api()->rest('GET', '/admin/api/2022-04/themes.json');
+        $shopThemes = $themes['body']['themes'];
+        $searchedThemeRole = "main";
         //search for the right theme id with the main role
         $activeTheme = array_filter(
             $shopThemes->toArray(),
@@ -43,26 +45,21 @@ Route::group(['middleware' => 'verify.shopify'], function () {
                 return $e['role'] == $searchedThemeRole;
             }
         );
-        
-
         $activeThemeId = $activeTheme[0]['id'];
-
-        $snippet = "Your snippet code updated 3";
+        $snippet = "{% section 'product-template' %}";
         //Snippet to pass to rest api request
         $data = array(
             'asset'=> [
-                'key' => 'snippets/lskit_codes.liquid', 
+                'key' => 'templates/product.lskit_theme_edit.liquid', 
                 'value' => $snippet
             ]
         );
-
         $shop->api()->rest('PUT', '/admin/api/2022-04/themes/'.$activeThemeId.'/assets.json', $data);
+        // get shop active currency
+        $shop_data = $shop->api()->rest('GET', '/admin/api/2022-04/shop.json');
+        $shop_active_currency = $shop_data['body']->shop->currency;
         // Save activated shop
-        return Setting::updateOrCreate([
-            'shop_id' => $shop->name,
-            'shop_active_theme_id' => $activeThemeId,
-            'activated' => true,
-        ]) ?  ['message' => 'Theme setup successfully'] : ['message' => 'Theme setup error!'];
+        return "Scuccessful installed";
 
 
     })->name('test');
